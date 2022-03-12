7482ca837bb58f73c941eeb34725eb7c502f193b
Absozero <aswsub123@gmail.com>
2022-03-11 21:59:24 -0800

    git fixes

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='Cliweather',
+    version='0.1.0',
+    install_requires=[
+        'Click',
+        'requests'
+    ],
+    entry_points={
+        'console_scripts': [
+            'cliw=src.main:main',
+        ],
+    },
+)
