f09b888f6fef14b19bd05726e4df06db6e9f9318
Gitea <gitea@fake.local>
2022-03-15 17:29:49 -0700

    Cli shud work now

--- cliweather/main.py
+++ cliweather/main.py
@@ -8,16 +8,16 @@ weath_gov = "https://api.weather.gov/"
 usweath = requests.get(weath_gov)
 
 
-@click.command()
-def main():
-    click.secho("hello", fg='blue')
+@click.group()
+def cli():
+    pass
 
-@click.command()
+@cli.command()
 def us():
     click.secho(usweath.json(), fg='green')
 
 if __name__ == '__main__':
-    main()
+    cli()
 
 
 
--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name='Cliweather',
     version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'Click',
         'requests'
     ],
     entry_points={
         'console_scripts': [
-            'cliw=cliweather.cli:main',
+            'cliw = cliweather.main:cli',
         ],
     },
 )
