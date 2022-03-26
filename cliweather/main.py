70607f2fbf12ba5aaa1157d97c6949b1bacfeed0
Gitea <gitea@fake.local>
2022-03-26 13:52:46 -0700

    subcommand addition

--- cliweather/main.py
+++ cliweather/main.py
@@ -1,5 +1,4 @@
 import requests
-import sys
 import click
 
 
@@ -10,11 +9,22 @@ usweath = requests.get(weath_gov)
 
 @click.group()
 def cli():
+    # This is the main function that will be called when the program is run
+    # It is the entry point of the program, as well as the command line interface.
+    # It is a gro
     pass
 
 @cli.command()
 def us():
     click.secho(usweath.json(), fg='green')
+
+@cli.group()
+def ca():
+    pass
+
+@ca.command()
+def toronto():
+    click.secho("Toronto", fg='green')
 
 if __name__ == '__main__':
     cli()
